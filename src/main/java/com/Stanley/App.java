a91b667e881159bbd9d3d70c583c85cd799acb82
Stanley Law <lawkalaung@gmail.com>
2019-03-06 22:32:36 +0800

    first commit

---
+++ src/main/java/com/Stanley/App.java
@@ -0,0 +1,13 @@
+package com.Stanley;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
